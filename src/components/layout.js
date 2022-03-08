d1341c7256fb575e9cb76a9a9c9b98a1fd0338cf
SeraGabor <sragabor@gmail.com>
2022-03-08 16:11:33 +0100

    change helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,16 +21,32 @@ export default function Layout({ children }) {
 
                 <meta charSet="utf-8" />
                 <title>Juno - Smart Contracts Zone</title>
-                <meta content="Juno is an interoperable smart contract network and a zone part of the Cosmos Network. Highly scalable, robust, secure and easy to deploy!" name="description"/>
+                <meta name={'title'} content={'Juno - Smart Contracts Zone'} />
+                <meta name={'description'} content={'Juno is an interoperable smart contract network and a zone part of the Cosmos Network. Highly scalable, robust, secure and easy to deploy!'} />
+
                 <meta content="Smart Contracts Zone" property="og:site_name"/>
                 <meta content="https://junonetwork.io/" property="og:url"/>
+                <meta property="og:title" content="Juno - Smart Contracts Zone" />
+                <meta name={'og:description'} content={'Juno is an interoperable smart contract network and a zone part of the Cosmos Network. Highly scalable, robust, secure and easy to deploy!'} />
                 <meta content={ogimage} property="og:image"/>
+
+                <link rel="canonical" href="https://junonetwork.io/" />
 
                 <link rel="apple-touch-icon" href={favicon} />
                 <link rel="apple-touch-icon" sizes="72x72" href={favicon72} />
 
-                <link rel="canonical" href="https://junonetwork.io/" />
                 <meta name="theme-color" content="#312526"/>
+
+                <meta property="article:publisher" content="https://junonetwork.io/" />
+                <meta property="og:site_name" content="Juno - Smart Contracts Zone" />
+                <meta property="og:type" content="website" />
+                <meta name="author" content="Juno - Smart Contracts Zone" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:text:title" content="Juno - Smart Contracts Zone" />
+                <meta property="og:url" content="https://junonetwork.io/" />
+
+                <meta property="og:image" content={ogimage} key="ogimage" />
+                <meta name="twitter:image" content={ogimage} key="twimage" />
 
             </Helmet>
             <Header/>
